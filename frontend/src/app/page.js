c3f7ce55e087091a760748a01f64d4e08dde0d0d
Minhaj Ap <minhajap00@gmail.com>
2024-06-25 18:57:36 +0530

    created home page.

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,11 @@
-export default function HomePage(){
-    return <h1>Hello, Next.js!</h1>
+import "@/styleSheets/homePage.css";
+export default function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="homePage-heading">
+        <h1>Welcome to Subscription Management App</h1>
+        <h2>SUBSCRIBE TO YOUR FAVORITES NOW!!!</h2>
+      </div>
+    </div>
+  );
 }
